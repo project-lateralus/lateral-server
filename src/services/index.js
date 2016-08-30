b5c26238f66d9a2d2dd81c8b4095616401bda6f5
Raul Pino <pinox101@gmail.com>
2016-08-30 16:25:13 -0300

    local fb credentials added, view renderer service implemented

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,16 @@
 'use strict';
+const mongoose = require('mongoose');
 const authentication = require('./authentication');
 const user = require('./user');
-const mongoose = require('mongoose');
+const viewsRenderer = require('./viewsRenderer');
+
 module.exports = function() {
   const app = this;
-  
+
   mongoose.connect(app.get('mongodb'));
   mongoose.Promise = global.Promise;
-  
+
   app.configure(authentication);
   app.configure(user);
+  app.configure(viewsRenderer);
 };
