2e191f1043a8465fee8af0f9031c4b23f2b7c3e5
Raul Pino <pinox101@gmail.com>
2016-08-31 20:25:07 -0300

    resolve api url from config var

--- src/services/viewsRenderer/index.js
+++ src/services/viewsRenderer/index.js
@@ -3,11 +3,12 @@
 // TODO use pug instead http://expressjs.com/en/guide/using-template-engines.html
 module.exports = function() {
   const app = this;
+  const apiUrl = app.get('frontend');
 
   app.set('view engine', 'jade');
   app.set('views', __dirname + '/views');
 
   app.get('/auth/facebook_success', (req, res) => {
-    res.render('facebookSuccess')
+    res.render('facebookSuccess', {apiUrl: apiUrl});
   });
 };
